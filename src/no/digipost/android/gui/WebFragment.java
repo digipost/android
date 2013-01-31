7649a56685170a7d82793e871f8498b28c722cf5
Fredrik Lillejordet <fretholil@gmail.com>
2013-01-31 11:04:26 +0100

    Fjernet utskrift av refreshtoken

--- src/no/digipost/android/gui/WebFragment.java
+++ src/no/digipost/android/gui/WebFragment.java
@@ -97,7 +97,6 @@ public class WebFragment extends DialogFragment {
 					Editor editor = settings.edit();
 					editor.putString(ApiConstants.REFRESH_TOKEN, cipher);
 					editor.commit();
-					System.out.println("refresh decrypted" + ksa.decrypt(cipher));
 
 				} catch (InterruptedException e) {
 					e.printStackTrace();
