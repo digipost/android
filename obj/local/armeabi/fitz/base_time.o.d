24dcd545835baf81344cf6ae03cd89bf144ec096
Sigurd Hagen Falk <sigurd.falk@gmail.com>
2013-03-07 01:13:32 +0100

    fjernet obj

--- obj/local/armeabi/fitz/base_time.o.d
+++
@@ -1,2 +0,0 @@
-/cygdrive/c/Users/Sigurd/Desktop/DigipostPDF/mupdf-1.1-source/android/obj/local/armeabi/objs-debug/mupdfcore/../../fitz/base_time.o: \
- /cygdrive/c/Users/Sigurd/Desktop/DigipostPDF/mupdf-1.1-source/android/jni/../../fitz/base_time.c
