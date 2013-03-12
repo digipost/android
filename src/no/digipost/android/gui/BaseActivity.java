efc328a396312df7a1670a0318f1e01309f379d5
Fredrik Lillejordet <fretholil@gmail.com>
2013-03-12 15:46:21 +0100

    Fjernet onRestoreInstance

--- src/no/digipost/android/gui/BaseActivity.java
+++ src/no/digipost/android/gui/BaseActivity.java
@@ -144,21 +144,6 @@ public class BaseActivity extends FragmentActivity {
 		}
 	}
 
-	@Override
-	protected void onSaveInstanceState(final Bundle outState) {
-		super.onSaveInstanceState(outState);
-		outState.putInt(CURRENT_PAGE, pageListener.getCurrentPage());
-		System.out.println("save");
-	}
-
-	@Override
-	protected void onRestoreInstanceState(final Bundle savedInstanceState) {
-		super.onRestoreInstanceState(savedInstanceState);
-		int currentPage = savedInstanceState.getInt(CURRENT_PAGE);
-		mViewPager.setCurrentItem(currentPage);
-		System.out.println("load");
-	}
-
 	private void logOut() {
 		SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(this);
 		settings.edit().clear().commit();
