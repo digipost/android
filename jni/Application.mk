de0306131219a6e6aab027d21e611de96582b850
Fredrik Lillejordet <fretholil@gmail.com>
2013-06-27 10:51:52 +0200

    inkludert mupdf for x86

--- jni/Application.mk
+++ jni/Application.mk
@@ -1,6 +1,6 @@
 # The ARMv7 is significanly faster due to the use of the hardware FPU
 APP_PLATFORM=android-8
-APP_ABI := armeabi armeabi-v7a
+APP_ABI := armeabi armeabi-v7a x86
 ifdef NDK_PROFILER
 # The profiler doesn't seem to receive ticks when run on release code.
 # Accordingly, we need to build as debug - but this turns optimisations
