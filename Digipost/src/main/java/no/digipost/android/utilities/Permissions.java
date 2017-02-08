5fa3c49a691017632abcf7c7b07d8c91ac91bef0
Fredrik Lillejordet <fretholil@gmail.com>
2017-02-08 12:07:00 +0100

    fikset feil header

--- Digipost/src/main/java/no/digipost/android/utilities/Permissions.java
+++ Digipost/src/main/java/no/digipost/android/utilities/Permissions.java
@@ -6,12 +6,26 @@ import android.content.Context;
 import android.support.v4.app.ActivityCompat;
 
 /**
- * Created by fredrik lillejordet on 21/01/16.
+ /**
+ * Copyright (C) Posten Norge AS
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 public class Permissions {
     private final static int REQUEST_WRITE_EXTERNAL_STORAGE = 0;
 
     public static void requestWritePermissionsIfMissing(Context context, Activity activity){
+
         if(!FileUtilities.isStorageWriteAllowed(context)) {
             ActivityCompat.requestPermissions(activity,
                     new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
