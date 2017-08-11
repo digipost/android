21054364ab1560b4694b42023361d2d89a243050
Fredrik Lillejordet <fretholil@gmail.com>
2017-08-11 13:41:48 +0200

    fjernet ubrukt import

--- Digipost/src/main/java/no/digipost/android/gui/metadata/AppointmentView.java
+++ Digipost/src/main/java/no/digipost/android/gui/metadata/AppointmentView.java
@@ -17,7 +17,6 @@
 package no.digipost.android.gui.metadata;
 
 import android.Manifest;
-import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.Fragment;
 import android.content.ContentResolver;
