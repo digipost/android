8e8bf85b47ee8b694f9beefd0cdb91fc7bacb704
Fredrik Lillejordet <fretholil@gmail.com>
2017-09-07 16:29:17 +0200

    styrer permissions ved filnedlastning i webview

--- Digipost/src/main/java/no/digipost/android/gui/metadata/ExternalLinkWebview.java
+++ Digipost/src/main/java/no/digipost/android/gui/metadata/ExternalLinkWebview.java
@@ -16,14 +16,17 @@
 
 package no.digipost.android.gui.metadata;
 
+import android.Manifest;
+import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.DownloadManager;
-import android.content.DialogInterface;
+import android.content.*;
 import android.graphics.drawable.ColorDrawable;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.ActionBar;
@@ -35,15 +38,25 @@ import android.view.WindowManager;
 import android.webkit.*;
 import no.digipost.android.R;
 import no.digipost.android.utilities.DialogUtitities;
+import no.digipost.android.utilities.FileUtilities;
 import no.digipost.android.utilities.Permissions;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import static java.lang.String.format;
 
 public class ExternalLinkWebview extends AppCompatActivity{
+
+    private WebView webView;
+    private String fileName;
+    private String fileUrl;
+    private Activity activity;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_externallink_webview);
-
+        this.activity = activity;
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         final ActionBar actionBar = getSupportActionBar();
@@ -58,12 +71,13 @@ public class ExternalLinkWebview extends AppCompatActivity{
             }
         }
 
-        WebView webView = (WebView) findViewById(R.id.externallink_webview);
+        webView = (WebView) findViewById(R.id.externallink_webview);
         webView.getSettings().setJavaScriptEnabled(true);
         webView.getSettings().setDomStorageEnabled(true);
         Bundle bundle = getIntent().getExtras();
         String url = bundle.getString("url", "https://www.digipost.no");
         enableCookies(webView);
+
         webView.setWebViewClient(new WebViewClient() {
             @Override
             public void onPageFinished(WebView view, String url) {
@@ -73,33 +87,49 @@ public class ExternalLinkWebview extends AppCompatActivity{
                     actionBar.setSubtitle(view.getUrl());
                 }
             }
-
         });
-        webView.setDownloadListener(new DownloadListener() {
 
+        webView.setDownloadListener(new DownloadListener() {
             @Override
             public void onDownloadStart(String url, String userAgent, String content, String mimeType, long contentLength) {
+                fileName = URLUtil.guessFileName(url, content,mimeType);
+                fileUrl = url;
+
+                BroadcastReceiver onComplete = new BroadcastReceiver() {
+                    @Override
+                    public void onReceive(Context context, Intent intent) {
+                        showDownloadSuccessDialog(context);
+                    }
+                };
+                registerReceiver(onComplete, new IntentFilter(DownloadManager.ACTION_DOWNLOAD_COMPLETE));
                 if (!mimeType.equals("text/html")) {
-                    showDownloadDialog(url);
+                    showDownloadDialog();
                 }
             }
         });
-        webView.loadUrl("https://www.digipost.no/testbrev.pdf");
+
+        if(FileUtilities.isStorageWriteAllowed(this)){
+            webView.loadUrl("https://www.sundar.no/testbrev.pdf");
+        }else{
+            showPermissionsDialog();
+        }
     }
 
-    private void showDownloadDialog(final String url) {
+    private void showDownloadDialog() {
+        String hostUrl = "";
+        try {
+            hostUrl = new URL(fileUrl).getHost();
+        }catch (MalformedURLException e){
+            hostUrl = fileUrl;
+        }
+
         String title = getString(R.string.externallink_download_title);
-        String message = getString(R.string.externallink_download_message);
+        String message = format(getString(R.string.externallink_download_message), fileName, hostUrl);
 
         AlertDialog.Builder builder = DialogUtitities.getAlertDialogBuilderWithMessageAndTitle(this, message, title);
-
         builder.setPositiveButton(getString(R.string.externallink_download_title), new DialogInterface.OnClickListener() {
             public void onClick(final DialogInterface dialog, final int id) {
-                boolean writePermissionsAllowed = Permissions.requestWritePermissionsIfMissing(getApplicationContext(), getParent());
-                while(!writePermissionsAllowed) {
-                    writePermissionsAllowed = Permissions.requestWritePermissionsIfMissing(getApplicationContext(), getParent());
-                }
-                downloadFile(url);
+                downloadFile(fileUrl);
                 dialog.dismiss();
             }
         }).setCancelable(false).setNegativeButton(getString(R.string.abort), new DialogInterface.OnClickListener() {
@@ -112,11 +142,36 @@ public class ExternalLinkWebview extends AppCompatActivity{
         builder.create().show();
     }
 
+    private void showPermissionsDialog() {
+        String message = getString(R.string.externallink_permissions);
+        AlertDialog.Builder builder = DialogUtitities.getAlertDialogBuilderWithMessage(this, message);
+        builder.setPositiveButton(getString(R.string.ok), new DialogInterface.OnClickListener() {
+            public void onClick(final DialogInterface dialog, final int id) {
+                Permissions.requestWritePermissionsIfMissing(getBaseContext(), activity);
+                dialog.dismiss();
+            }
+        }).setCancelable(false).setNegativeButton(getString(R.string.abort), new DialogInterface.OnClickListener() {
+            public void onClick(final DialogInterface dialog, final int id) {
+                dialog.cancel();
+                finish();
+            }
+        });
+
+        builder.create().show();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(FileUtilities.isStorageWriteAllowed(this)){
+            webView.loadUrl("https://www.digipost.no/testbrev.pdf");
+        }
+    }
+
     private void downloadFile(final String url) {
         DownloadManager.Request request = new DownloadManager.Request(Uri.parse(url));
-        String IDPORTEN = "https://idporten.difi.no";
-        String idPortenSession = CookieManager.getInstance().getCookie(IDPORTEN);
-        if (idPortenSession != null) request.addRequestHeader(IDPORTEN, idPortenSession);
+        String idPortenSession = CookieManager.getInstance().getCookie("https://idporten.difi.no");
+        if (idPortenSession != null) request.addRequestHeader("Cookie", idPortenSession);
         request.allowScanningByMediaScanner();
         request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
         request.setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, url);
@@ -125,10 +180,9 @@ public class ExternalLinkWebview extends AppCompatActivity{
         dm.enqueue(request);
     }
 
-    private void showDownloadSuccessDialog() {
-        String message = "";
-        String title = "";
-        AlertDialog.Builder builder = DialogUtitities.getAlertDialogBuilderWithMessageAndTitle(getApplicationContext(),message,title);
+    private void showDownloadSuccessDialog(final Context downloadContext) {
+        String message = format(getString(R.string.externallink_download_success_message), fileName);
+        AlertDialog.Builder builder = DialogUtitities.getAlertDialogBuilderWithMessage(downloadContext, message);
         builder.setPositiveButton(getString(R.string.ok), new DialogInterface.OnClickListener() {
             public void onClick(final DialogInterface dialog, final int id) {
                 dialog.dismiss();
