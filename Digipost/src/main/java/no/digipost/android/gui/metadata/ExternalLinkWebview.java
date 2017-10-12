07d7607e5bb8d09d3d97ff90529f3ec7174aa5fc
Fredrik Lillejordet <fretholil@gmail.com>
2017-10-12 12:26:01 +0200

    fikset formatering etter tips i PR

--- Digipost/src/main/java/no/digipost/android/gui/metadata/ExternalLinkWebview.java
+++ Digipost/src/main/java/no/digipost/android/gui/metadata/ExternalLinkWebview.java
@@ -1,12 +1,12 @@
 /**
  * Copyright (C) Posten Norge AS
- *
+ * <p>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
+ * <p>
  * http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -39,12 +39,13 @@ import no.digipost.android.R;
 import no.digipost.android.utilities.DialogUtitities;
 import no.digipost.android.utilities.FileUtilities;
 import no.digipost.android.utilities.Permissions;
+
 import java.net.MalformedURLException;
 import java.net.URL;
 
 import static java.lang.String.format;
 
-public class ExternalLinkWebview extends AppCompatActivity{
+public class ExternalLinkWebview extends AppCompatActivity {
 
     private WebView webView;
     private String fileName;
@@ -61,6 +62,7 @@ public class ExternalLinkWebview extends AppCompatActivity{
         if (actionBar != null) {
             actionBar.setDisplayHomeAsUpEnabled(true);
             actionBar.setBackgroundDrawable(new ColorDrawable(0xff454545));
+
             if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
                 Window window = this.getWindow();
                 window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
@@ -80,7 +82,7 @@ public class ExternalLinkWebview extends AppCompatActivity{
             @Override
             public void onPageFinished(WebView view, String url) {
                 super.onPageFinished(view, url);
-                if(actionBar != null) {
+                if (actionBar != null) {
                     actionBar.setTitle(view.getTitle());
                     actionBar.setSubtitle(view.getUrl());
                 }
@@ -101,18 +103,18 @@ public class ExternalLinkWebview extends AppCompatActivity{
 
                 registerReceiver(onComplete, new IntentFilter(DownloadManager.ACTION_DOWNLOAD_COMPLETE));
                 if (!mimeType.equals("text/html")) {
-                    if(FileUtilities.isStorageWriteAllowed(getApplicationContext())) {
+                    if (FileUtilities.isStorageWriteAllowed(getApplicationContext())) {
                         showDownloadDialog(userAgent, content, mimeType, contentLength);
-                    }else{
+                    } else {
                         showMissingPermissionsDialog();
                     }
                 }
             }
         });
 
-        if(FileUtilities.isStorageWriteAllowed(this)){
+        if (FileUtilities.isStorageWriteAllowed(this)) {
             webView.loadUrl(fileUrl);
-        }else{
+        } else {
             showPermissionsDialog();
         }
     }
@@ -120,7 +122,7 @@ public class ExternalLinkWebview extends AppCompatActivity{
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        if(onComplete != null) {
+        if (onComplete != null) {
             unregisterReceiver(onComplete);
         }
     }
@@ -129,7 +131,7 @@ public class ExternalLinkWebview extends AppCompatActivity{
         String hostUrl = "";
         try {
             hostUrl = new URL(fileUrl).getHost();
-        }catch (MalformedURLException e){
+        } catch (MalformedURLException e) {
             hostUrl = fileUrl;
         }
 
@@ -178,7 +180,7 @@ public class ExternalLinkWebview extends AppCompatActivity{
         webView.loadUrl(fileUrl);
     }
 
-    private void showMissingPermissionsDialog(){
+    private void showMissingPermissionsDialog() {
         String message = getString(R.string.externallink_permissions);
         AlertDialog.Builder builder = DialogUtitities.getAlertDialogBuilderWithMessage(this, message);
         builder.setPositiveButton(getString(R.string.ok), new DialogInterface.OnClickListener() {
@@ -215,7 +217,7 @@ public class ExternalLinkWebview extends AppCompatActivity{
         builder.create().show();
     }
 
-    private void enableCookies(WebView webView){
+    private void enableCookies(WebView webView) {
         CookieManager.getInstance().setAcceptCookie(true);
         if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP) {
             CookieManager.getInstance().setAcceptThirdPartyCookies(webView, true);
