a0ecb4faa3706bebdde80c7fd27555f9f20b0979
Lars Kristian Haga <LarsKHaga@gmail.com>
2019-01-13 23:04:38 +0100

    Tweeked the approach to get the application running, much more intuitive now.

---
+++ Digipost/src/main/java/no/digipost/android/authentication/Secret.java
@@ -0,0 +1,12 @@
+package no.digipost.android.authentication;
+public class Secret {
+    // Real values can ge gotten from; https://www.digipost.no/plattform/annet/oauth/
+    // TODO: Change value below to a real value
+    public static final String CLIENT_ID = "";
+    // TODO: Change value below to a real value
+    public static final String CLIENT_SECRET = "";
+    // TODO: Change value below to a real value
+    public static final String REDIRECT_URI = "";
+
+    public static final String ENV_URL = "https://www.digipost.no/post/";
+}
